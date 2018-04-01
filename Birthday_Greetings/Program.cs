670d090c96552c45575036390007f269b3628622
屹善 赵 <franklinzhao123@hotmail.com>
2018-04-01 13:51:01 +0800

    Final edition

--- Birthday_Greetings/Program.cs
+++ Birthday_Greetings/Program.cs
@@ -1,65 +1,1052 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using SupplementaryClassLibraryForStringManipulation;
 using System.Threading;
 
 namespace Birthday_Greetings
 {
 	class Program
 	{
-		static void Main(string[] args)
+		const string test = "SEKAI-DE-ICHIPAN O-HIME-SAMA ";
+
+		const string greeting = "HAPPY BIRTHDAY";
+
+		public static void Main(string[] args)
 		{
-			WriteLetter(Letters.H);
+			initialization();
+
+			if (Convert.ToDateTime(DateTime.Now.ToShortDateString()) < Convert.ToDateTime("2018/8/31"))
+			{
+				Console.WriteLine("还没有到查看的时间哦\n");			
+			}
+			else
+			{
+				Thread.Sleep(9000);
+				for (int i = 1; i <= StringManipulation.Len(test); i++)
+				{
+					WriteLetter(StringManipulation.Mid(test, i, 1));
+				}
+
+				WriteLetter(" ");
+
+				Console.WriteLine("生日快乐哦\n");
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				Console.Write(".");
+
+				Thread.Sleep(5000);
+			}
+
+			Console.WriteLine("\n");
+
+			Console.WriteLine("按Enter键退出");
 
 			Console.ReadKey();
 		}
 
-		static void WriteLetter(Letters letter)
+		private static void initialization()
+		{
+			Console.CursorVisible = false;
+			Console.WindowHeight = Console.LargestWindowHeight;
+			Console.WindowWidth = Console.LargestWindowWidth;
+			Console.WindowTop = 0;
+			Console.WindowLeft = 0;
+		}
+
+		private static void WriteLetter(string letter)
 		{
 			switch (letter)
 			{
-				case Letters.H:
+				#region A
+
+				case "A":
+
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.YCoord--;
+
+					Cursor.YCoord -= 4;
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.YCoord--;
+
+					Cursor.YCoord -= 2;
+					Cursor.XCoord -= 4;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 6;
+					Cursor.YCoord -= 2;
+
+					break;
+
+				#endregion
+
+				#region B
+
+				case "B":
 
 					WriteVertical();
 
-					
+					Cursor.YCoord -= 4;
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 8;
+					Cursor.YCoord -= 4;
 
 					break;
-				case Letters.A:
+
+				#endregion
+
+				#region C
+
+				case "C":
+
+					Cursor.XCoord += 6;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord -= 2;
+					}
+
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord += 2;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 4;
+
 					break;
-				case Letters.P:
+
+				#endregion
+
+				#region D
+
+				case "D":
+
+					WriteVertical();
+
+					Cursor.YCoord -= 4;
+					Cursor.XCoord += 2;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 8;
+					Cursor.YCoord -= 4;
+
 					break;
-				case Letters.Y:
+
+				#endregion
+
+				#region E
+
+				case "E":
+
+					WriteLetter("F");
+
+					Cursor.XCoord -= 8;
+					Cursor.YCoord += 4;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.YCoord -= 4;
+					Cursor.XCoord += 2;
+
 					break;
-				case Letters.B:
+
+				#endregion
+
+				#region F
+
+				case "F":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 4;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.XCoord -= 6;
+					Cursor.YCoord += 2;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 6;
+					Cursor.YCoord -= 2;
+
 					break;
-				case Letters.I:
+
+				#endregion
+
+				#region G
+
+				case "G":
+
+					Cursor.XCoord += 6;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord -= 2;
+					}
+
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord += 2;
+
+					for (int i = 0; i < 2; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
 					break;
-				case Letters.R:
+
+				#endregion
+
+				#region H
+
+				case "H":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 2;
+
+					WriteVertical();
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
 					break;
-				case Letters.T:
+
+				#endregion
+
+				#region I
+
+				case "I":
+
+					WriteVertical();
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
 					break;
-				case Letters.D:
+
+				#endregion
+
+				#region J
+
+				case "J":
+
+					Cursor.XCoord += 2;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 6;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region K
+
+				case "K":
+
+					WriteVertical();
+
+					Cursor.XCoord += 6;
+					Cursor.YCoord -= 4;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord -= 2;
+						Cursor.YCoord++;						
+					}
+
+					Cursor.XCoord += 4;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+					
+					break;
+
+				#endregion
+
+				#region L
+
+				case "L":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region M
+
+				case "M":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 3;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					WriteVertical();
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region N
+
+				case "N":
+
+					WriteVertical();
+
+					Cursor.YCoord -= 4;
+					for (int i = 0; i < 3; i++)
+					{
+						Cursor.XCoord += 2;
+						Cursor.YCoord++;
+
+						Console.Write("■");
+						Thread.Sleep(150);
+					}
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord += 2;
+
+					for (int i = 0; i < 5; i++)
+					{
+						Cursor.YCoord--;
+
+						Console.Write("■");
+
+						Thread.Sleep(150);												
+					}
+
+					Cursor.XCoord += 4;
+
+					break;
+
+				#endregion
+
+				#region O
+
+				case "O":
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Cursor.XCoord -= 2;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord--;
+					}
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 6;
+
+					break;
+
+				#endregion
+
+				#region P
+
+				case "P":
+
+					WriteVertical();
+
+					Cursor.YCoord -= 4;
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Cursor.YCoord++;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 8;
+					Cursor.YCoord -= 2;
+
+					break;
+
+				#endregion
+
+				#region Q
+
+				case "Q":
+
+					WriteLetter("O");
+
+					Cursor.XCoord -= 4;
+					Cursor.YCoord += 4;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region R
+
+				case "R":
+
+					WriteLetter("P");
+
+					Cursor.XCoord -= 6;
+					Cursor.YCoord += 3;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+					
+				#region S
+
+				case "S":
+
+					Cursor.XCoord += 6;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord -= 2;
+					}
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					for (int i = 0; i < 3; i++)
+					{
+						Cursor.XCoord -= 2;
+
+						Console.Write("■");
+
+						Thread.Sleep(150);
+					}
+
+					Cursor.XCoord += 10;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region T
+
+				case "T":
+
+					for (int i = 0; i < 5; i++)
+					{
+						Console.Write("■");
+						Thread.Sleep(150);
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.XCoord -= 6;
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.YCoord--;
+
+					Cursor.XCoord += 8;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region U
+
+				case "U":
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.YCoord++;
+					}
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					for (int i = 0; i < 5; i++)
+					{
+						Cursor.YCoord--;
+
+						Console.Write("■");
+
+						Thread.Sleep(150);
+					}
+
+					Cursor.XCoord += 4;
+
+					break;
+
+				#endregion
+
+				#region V
+
+				case "V":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+
+					break;
+
+				#endregion
+
+				#region W
+
+				case "W":
+
+					WriteVertical();
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord--;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord += 2;
+					for (int i = 0; i < 5; i++)
+					{
+						Cursor.YCoord--;
+
+						Console.Write("■");
+
+						Thread.Sleep(150);
+					}
+
+					Cursor.XCoord += 4;
+
+					break;
+
+				#endregion
+
+				#region X
+
+				case "X":
+
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord -= 4;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 4;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 12;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region Y
+
+				case "Y":
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 4;
+					Cursor.YCoord -= 2;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord -= 2;
+					Cursor.YCoord++;
+
+					Console.Write("■");
+					Thread.Sleep(150);
+					Cursor.YCoord++;
+					Console.Write("■");
+					Thread.Sleep(150);
+
+					Cursor.XCoord += 8;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+
+				#region Z
+
+				case "Z":
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+						
+						Cursor.XCoord += 2;
+					}
+					Cursor.XCoord -= 2;
+					Cursor.YCoord++;
+
+					for (int i = 0; i < 3; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord -= 2;
+						Cursor.YCoord++;
+					}
+
+					for (int i = 0; i < 4; i++)
+					{
+						Console.Write("■");
+
+						Thread.Sleep(150);
+
+						Cursor.XCoord += 2;
+					}
+
+					Cursor.XCoord += 2;
+					Cursor.YCoord -= 4;
+
+					break;
+
+				#endregion
+					
+				#region " "
+
+				case " ":
+
+					Cursor.XCoord = 0;
+					Cursor.YCoord += 7;
+
+					break;
+
+				#endregion
+
+				default:
+
+					Cursor.XCoord += 12;
+
 					break;
 			}
 		}
 
-		static void WriteVertical()
+		private static void WriteVertical()
 		{
 			for (int i = 0; i < 5; i++)
 			{
-				Pointer.YCoord = i;
-
-				Console.Write("█");
+				Console.Write("■");
 
 				Thread.Sleep(150);
 
+				Cursor.YCoord++;
 			}
+
+			Cursor.YCoord--;
 		}
 	}
 
-	class Pointer
+	class Cursor
 	{
 		private static int xCoord = 0;
 
@@ -93,11 +1080,5 @@ namespace Birthday_Greetings
 				Console.SetCursorPosition(xCoord, YCoord);
 			}
 		}
-	}
-	
-	enum Letters
-	{
-		H, A, P, Y, 
-		B, I, R, T, D
 	}
 }
---
+++ SupplementaryClassLibraryForStringManipulation/Class1.vb
@@ -0,0 +1,31 @@
+﻿Public Class StringManipulation
+
+	Public Shared Function Len(input As String) As Int64
+
+		Return Strings.Len(input)
+
+	End Function
+
+	Public Shared Function Mid(input As String, start As Integer, length As Integer) As String
+
+		If start <= 0 Then
+
+			Throw New ArgumentOutOfRangeException()
+
+			Return input
+
+		End If
+
+		If length > Strings.Len(input) Then
+
+			Return input
+
+		Else
+
+			Return Strings.Mid(input, start, length)
+
+		End If
+
+	End Function
+
+End Class
